8c851f67f4eea065beb4369dadd18dd3dfb7eb15
Herald <heraldcnp@gmail.com>
2024-05-22 10:40:14 -0400

    add changes recibo

--- src/app/dashboard/pages/planillas/components/print-recibo/print-recibo.component.ts
+++ src/app/dashboard/pages/planillas/components/print-recibo/print-recibo.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject, inject, signal } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { AuthService } from 'src/app/auth/services/auth.service';
 import { BeneficiariesService } from 'src/app/dashboard/services/beneficiaries.service';
 import { PlanillaService } from 'src/app/dashboard/services/planilla.service';
 
@@ -14,19 +15,29 @@ export class PrintReciboComponent {
   private fb = inject(FormBuilder);
   private planillaService = inject(PlanillaService);
   private beneficiaryService = inject(BeneficiariesService);
+  private authService = inject(AuthService);
+
+  idUser: any;
+  user: any;
+  dataUser: any;
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: any, private ref: MatDialogRef<PrintReciboComponent>) {
-
+    this.user = localStorage.getItem('user');
+    this.dataUser = this.authService.currentUser();
+    this.idUser = this.data.id;
   }
+
+
   inputData: any;
   habilitados = signal<any>(null);
   planilla = signal<any>(null);
   beneficiaryPagado = signal<any>(null);
   beneficiary = signal<any>(null);
+  date = new Date();
 
   ngOnInit(): void {
     this.inputData = this.data;
-    if(this.inputData.idPlanilla && this.inputData.idPersona){
+    if (this.inputData.idPlanilla && this.inputData.idPersona) {
       console.log('idPlanilla', this.inputData.idPlanilla);
       console.log('idPersona', this.inputData.idPersona);
     }
@@ -34,35 +45,35 @@ export class PrintReciboComponent {
     this.getPlanilla();
   }
 
-  getBeneficiary(){
+  getBeneficiary() {
     this.beneficiaryService.getBeneficiaryById(this.inputData.idPersona).subscribe(item => {
       this.beneficiary.set(item);
       console.log('Beneficiario', this.beneficiary());
     })
   }
 
-  getPlanilla(){
+  getPlanilla() {
     this.planillaService.getPlanilla(this.inputData.idPlanilla)
-    .subscribe({
-      next: (data: any) => {
-        this.habilitados.set(data);
-        this.planilla.set(data);
-        console.log('planilla', this.planilla());
+      .subscribe({
+        next: (data: any) => {
+          this.habilitados.set(data);
+          this.planilla.set(data);
+          console.log('planilla', this.planilla());
 
-        // console.log(this.habilitados().Habilitados);
-        this.beneficiaryPagado.set(this.sacarBeneficiario(this.inputData.idPersona));
-        console.log('Beneficiario Pagado', this.beneficiaryPagado());
+          // console.log(this.habilitados().Habilitados);
+          this.beneficiaryPagado.set(this.sacarBeneficiario(this.inputData.idPersona));
+          console.log('Beneficiario Pagado', this.beneficiaryPagado());
 
-      }
-    })
+        }
+      })
   }
 
-  sacarBeneficiario(idBene:any) {
+  sacarBeneficiario(idBene: any) {
     console.log(idBene);
     // Encontrar el objeto por el id
-    let  encontrado;
+    let encontrado;
     this.habilitados().Habilitados.forEach((b: { id: any; }) => {
-      if(b.id === idBene) {
+      if (b.id === idBene) {
         encontrado = b;
       }
     });
